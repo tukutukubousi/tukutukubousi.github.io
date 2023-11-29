3831fb1f9e22526d7859a34bb620b119a1c381f6
tukutukubousi <137376608+tukutukubousi@users.noreply.github.com>
2023-11-29 11:16:18 +0900

    Create main.js

---
+++ index/main.js
@@ -0,0 +1,14 @@
+$(window).on('load', function () {
+  $("#splash-logo").delay(1200).fadeOut('slow');
+  //ロゴを1.2秒でフェードアウト
+
+  $("#splash").delay(1500).fadeOut('slow', function () {//splashエリアを1.5秒でフェードアウト
+
+    $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+
+  });
+
+
+});
+
+
